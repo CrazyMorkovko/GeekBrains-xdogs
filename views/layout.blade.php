7683b3ae9338590f5baeccd161a633eda17c5072
crazymorkovko <reginasakhapova@yandex.ru>
2018-04-23 01:18:14 +0300

    remove useless code

--- views/layout.blade.php
+++ views/layout.blade.php
@@ -50,37 +50,6 @@
                 <div class="container center"><h1>Одежда для собак</h1></div>
             @endif
         </header>
-
-        {{--@if($page != 'main')--}}
-            {{--<header class="header">--}}
-                {{--<div class="container">--}}
-                    {{--<a href="/index.php" class="header__logo-link">--}}
-                        {{--<div class="header__logo"></div>--}}
-                        {{--<div class="header__brand">XDogs</div>--}}
-                    {{--</a>--}}
-                    {{--<ul class="menu">--}}
-                        {{--@include('links')--}}
-                    {{--</ul>--}}
-                    {{--<div class="clearfix"></div>--}}
-                {{--</div>--}}
-            {{--</header>--}}
-        {{--@else--}}
-            {{--<div class="main-banner">--}}
-                {{--<header class="header">--}}
-                    {{--<div class="container">--}}
-                        {{--<a href="/index.php" class="header__logo-link header__logo-link--banner">--}}
-                            {{--<div class="header__logo header__logo--banner"></div>--}}
-                            {{--<div class="header__brand header__brand--banner">XDogs</div>--}}
-                        {{--</a>--}}
-                        {{--<ul class="menu menu--banner">--}}
-                            {{--@include('links')--}}
-                        {{--</ul>--}}
-                        {{--<div class="clearfix"></div>--}}
-                    {{--</div>--}}
-                {{--</header>--}}
-                {{--<div class="container center"><h1>Одежда для собак</h1></div>--}}
-            {{--</div>--}}
-        {{--@endif--}}
         <div class="content">
             <div class="container">
                 @yield('content')
--- views/links.blade.php
+++
@@ -1,3 +0,0 @@
-<li><a href="/">Главная</a></li>
-<li><a href="index.php?page=catalog">Каталог</a></li>
-<li><a href="index.php?page=contacts">Контакты</a></li>
