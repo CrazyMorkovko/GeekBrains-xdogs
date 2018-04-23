1726790fc97653efb28825dae8c4a6ad7394946e
crazymorkovko <reginasakhapova@yandex.ru>
2018-04-24 00:20:18 +0300

    continue using Bootstrap

--- views/catalog.blade.php
+++ views/catalog.blade.php
@@ -3,14 +3,26 @@
 
 <h1>Каталог</h1>
 
-<div class="products">
+<div class="products row">
     @foreach ($products as $product)
-        <div class="product-card">
-            <a href="/index.php?page=product&id={{ $product['id'] }}">
-                <img src="{{ $product['img'] }}" alt="{{ $product['name'] }}">
-            </a>
-            <a href="/index.php?page=product&id={{ $product['id'] }}">{{ $product['name'] }}</a>
-            <p>{{ $product['shortDesc'] }}</p>
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <div class="card product-card">
+                <img class="card-img-top" src="{{ $product['img'] }}" alt="{{ $product['name'] }}">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $product['name'] }}</h5>
+                    <p class="card-text">{{ $product['shortDesc'] }}</p>
+                    <div class="row">
+                        <div class="col">
+                            <a href="/index.php?page=product&id={{ $product['id'] }}" class="btn btn-primary">
+                                Просмотр
+                            </a>
+                        </div>
+                        <div class="col d-flex align-items-center justify-content-end">
+                            <strong>{{ $product['price'] . " руб." }}</strong>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     @endforeach
 </div>
--- views/openProduct.blade.php
+++ views/openProduct.blade.php
@@ -3,12 +3,10 @@
 
     <div class="product">
         <h1>{{ $product['name'] }}</h1>
-        <div class="product__body">
+        <div class="product__body mb-2">
             <div>
-                <a href="{{ $product['img'] }}" target="_blank">
-                    <img class="product__img"
-                         src="{{ $product['img'] }}"
-                         alt="{{ $product['name'] }}">
+                <a href="#" data-toggle="modal" data-target="#enlargePic" class="product-img-link">
+                    <img class="product__img" src="{{ $product['img'] }}" alt="{{ $product['name'] }}">
                 </a>
             </div>
             <div class="product__short-desc">
@@ -30,49 +28,77 @@
                     @break
                 @endswitch
 
-                <div><a class="beauty-button">В корзину</a></div>
+                <button type="button" class="btn btn-success">В корзину</button>
             </div>
         </div>
         <h2>Характеристики товара</h2>
-        <table>
-            <thead>
-            <tr>
-                <th>Размер</th>
-                <th>Длина по спинке</th>
-                <th>Объем груди</th>
-            </tr>
-            </thead>
-            <tfoot>
-            <tr>
-                <td>Цена</td>
-                <td colspan="2"><strong>{{ $product['price'] }} рублей</strong></td>
-            </tr>
-            </tfoot>
-            <tbody>
-            @foreach ($product["attributes"] as $attribute)
-            <tr>
-                <td>{{ $attribute['size'] }}</td>
-                <td>{{ $attribute['width'] }}</td>
-                <td>{{ $attribute['volume'] }}</td>
-            </tr>
-            @endforeach
-            </tbody>
-        </table>
-
-        <h3>Наши преимущества:</h3>
-        <ol class="product__check-list">
-            <li>Низкие цены</li>
-            <li>Все вещи на сайте - в наличии</li>
-            <li>Ускоренная доставка заказа</li>
-            <li>Акции и бонусы для постоянных клиентов</li>
-        </ol>
-
+        <div class="row">
+            <div class="col-md-6">
+                <table class="table table-bordered table-hover">
+                    <thead>
+                    <tr>
+                        <th>Размер</th>
+                        <th>Длина по спинке</th>
+                        <th>Объем груди</th>
+                    </tr>
+                    </thead>
+                    <tfoot>
+                    <tr>
+                        <td>Цена</td>
+                        <td colspan="2"><strong>{{ $product['price'] }} рублей</strong></td>
+                    </tr>
+                    </tfoot>
+                    <tbody>
+                    @foreach ($product["attributes"] as $attribute)
+                        <tr>
+                            <td>{{ $attribute['size'] }}</td>
+                            <td>{{ $attribute['width'] }}</td>
+                            <td>{{ $attribute['volume'] }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-6">
+                <h3>Наши преимущества:</h3>
+                <ol class="product__check-list">
+                    <li>Низкие цены</li>
+                    <li>Все вещи на сайте - в наличии</li>
+                    <li>Ускоренная доставка заказа</li>
+                    <li>Акции и бонусы для постоянных клиентов</li>
+                </ol>
+            </div>
+        </div>
         <h2>Подробное описание товара</h2>
         <div class="product__desc">
             <p>{{ $product['desc'] }}</p>
         </div>
 
         <h2>Отзывы</h2>
+    </div>
+
+    <div class="modal fade"
+         id="enlargePic"
+         tabindex="-1"
+         role="dialog"
+         aria-labelledby="enlargePicLabel"
+         aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="enlargePicLabel">{{ $product['name'] }}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <img src="{{ $product['img'] }}" class="img-fluid">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+                </div>
+            </div>
+        </div>
     </div>
 
 @endsection
